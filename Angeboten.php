6fac5e168d704bac0ff19a472ffe304ff7868368
khemy27 <114742447+khemy27@users.noreply.github.com>
2022-10-05 14:31:27 +0200

    Add files via upload

--- Angeboten.php
+++ Angeboten.php
@@ -6,19 +6,19 @@ session_start();
             echo '<table border="1">';
             while ($datensatz = mysqli_fetch_assoc($rueckgabe)){
             
-  if($datum ='von 15102022 bis 20102022');
-  {
-  echo "Die aktuelle Angebote ist momentan von Bremen nach Hamburg"; 
-  echo "<br/>";
+  if($datum = "von 15102022 bis 20102022"){
+ 
+    echo "Die aktuelle Angebote ist momentan von Bremen nach Hamburg,schnell Buchen!".echo "<br/>";
+  }
+  if($plaetze = "von Guetersloh nach Marseille AND "von "von 15102022 bis 20102022"){
+    echo "Beim naechste Reise erste 20km kostenfrei!".echo "<br/>";
   }
   {
   echo '<table border="1">';
   while ($datensatz = mysqli_fetch_assoc($rueckgabe))
   }
       
-             
-            
-            
+                      
             
   ?>
 
